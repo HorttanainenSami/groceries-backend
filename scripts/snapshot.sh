84b54f21dff2857550fdbbfcbcfca47a814b106d
Sami Horttanainen <sami1horttanainen@gmail.com>
2026-06-24 14:05:28 +0300

    fix order of actions

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -7,10 +7,11 @@ set -e
 
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 SNAPSHOT_DIR="$HOME/backups"
-SNAPSHOT_FILE="$SNAPSHOT_DIR/backup-$TIMESTAMP.sql"
-VERSION_FILE="$SNAPSHOT_DIR/version-$TIMESTAMP.txt"
+SNAPSHOT_FILE="$SNAPSHOT_DIR/$TIMESTAMP/backup-$TIMESTAMP.sql"
+VERSION_FILE="$SNAPSHOT_DIR/$TIMESTAMP/version-$TIMESTAMP.txt"
 
 mkdir -p "$SNAPSHOT_DIR"
+mkdir -p "$SNAPSHOT_DIR/$TIMESTAMP"
 
 if ! docker info &> /dev/null; then
     echo "Error: Docker is not running"
@@ -18,4 +19,4 @@ if ! docker info &> /dev/null; then
 fi
 
 docker exec groceries-postgres-container pg_dump -U $POSTGRES_USER $POSTGRES_DB > "$SNAPSHOT_FILE"
-echo $1 > "$VERSION_FILE"
+cat ~/groceries-backend/current-version.txt > "$VERSION_FILE"
